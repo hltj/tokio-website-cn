ec35d446a44bdc18982fb60cc22a88124bc01929
hltj <jiaywe@gmail.com>
2018-09-21 08:22:00 +0800

    travis 自动发布

---
+++ deploy_cn.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "beta" ]]
+then
+  git clone https://github.com/davisp/ghp-import.git &&
+  ./ghp-import/ghp_import.py -n -p -f -m "内容更新" -r https://"$TOKIOCN_GH_TOKEN"@github.com/tokio-cn/tokio-cn.github.io.git public &&
+  echo "内容已更新"
+fi
