b1c374f2f617eff0eb3e0e762ea12b2212d1bde4
hltj <jiaywe@gmail.com>
2018-09-21 08:46:48 +0800

    修正自动发布条件

--- deploy_cn.sh
+++ deploy_cn.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$TRAVIS_OS_NAME" == "linux" && "$TRAVIS_RUST_VERSION" == "beta" ]]
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
   git clone https://github.com/davisp/ghp-import.git &&
   ./ghp-import/ghp_import.py -n -p -f -m "内容更新" -r https://"$TOKIOCN_GH_TOKEN"@github.com/tokio-cn/tokio-cn.github.io.git public &&
