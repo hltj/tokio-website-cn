d21f859872dc9f962a3e400537fee9412fe3a9e2
hltj <jiaywe@gmail.com>
2018-09-21 10:14:15 +0800

    修正自动发布目标分支

--- deploy_cn.sh
+++ deploy_cn.sh
@@ -2,7 +2,10 @@
 
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]
 then
+  git clone https://github.com/tokio-cn/tokio-cn.github.io.git ../website
+  mv public ../website/
+  cd ../website
   git clone https://github.com/davisp/ghp-import.git &&
-  ./ghp-import/ghp_import.py -n -p -f -m "内容更新" -r https://"$TOKIOCN_GH_TOKEN"@github.com/tokio-cn/tokio-cn.github.io.git public &&
+  ghp-import/ghp_import.py -n -p -f -m "内容更新" -b master -r https://"$TOKIOCN_GH_TOKEN"@github.com/tokio-cn/tokio-cn.github.io.git public &&
   echo "内容已更新"
 fi
